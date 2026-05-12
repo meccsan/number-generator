15a3d8c0180de862bad9738933f77dd7eff5be64
Alexander Conrad <conrad.alex55@gmail.com>
2026-05-11 22:30:52 -0500

    added comments to main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ def main():
     try:
         i = int(input("Give first number (for range): "))
         j = int(input("Give second number (for range): "))
-        if i > j:
-            i, j = j, i
+        if i > j:           # If the user enters the first number
+            i, j = j, i     # and the first number is smaller than second, swap them
     except ValueError:
         print("That's not a legal number")
         return
 
-    randnum = random.randint(i, j)
+    randnum = random.randint(i, j)  # set the range of randnum to the values of i and j
     
-    while True:
+    while True: # the main loop of guessing until randnum == usrguess
         usrguess = int(input(f"Guess a number between {i} and {j}: "))
 
         if usrguess < randnum:
@@ -23,5 +23,6 @@ def main():
             print("That's right")
             break
 
+# prevent importation errors
 if __name__ == "__main__":
     main()
